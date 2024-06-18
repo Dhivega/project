ed1372e076f21dafb7282f612c5c7340ca8e2a1c
Dhivega <104583245+Dhivega@users.noreply.github.com>
2024-06-18 23:45:41 +0530

    Delete schemas directory

--- schemas/status.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `status` (
-	`status_id` INT(11) NULL DEFAULT NULL,
-	`status` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8_general_ci'
-)
-COLLATE='utf8_general_ci'
-ENGINE=InnoDB
-;
--- schemas/user_role.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `user_role` (
-	`id` INT(50) NULL DEFAULT NULL,
-	`user_id` INT(50) NULL DEFAULT NULL,
-	`role_id` INT(50) NULL DEFAULT NULL
-)
-COLLATE='utf8_general_ci'
-ENGINE=InnoDB
-;
